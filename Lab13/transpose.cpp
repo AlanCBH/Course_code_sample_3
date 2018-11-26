5daa36031e732d6edd87c472c06eecc979eb2654
Binghui Cheng <binghui2@siebl-0220-04.ews.illinois.edu>
2018-11-26 16:41:47 -0600

    part1

--- Lab13/transpose.cpp
+++ Lab13/transpose.cpp
@@ -9,11 +9,37 @@
 using std::min;
 
 // modify this function to add tiling
+// void
+// transpose_tiled(int **src, int **dest) {
+//     for (int i = 0; i < SIZE; i ++) {
+//         for (int j = 0; j < SIZE; j ++) {
+//             dest[i][j] = src[j][i];
+//         }
+//     }
+// }
 void
 transpose_tiled(int **src, int **dest) {
-    for (int i = 0; i < SIZE; i ++) {
-        for (int j = 0; j < SIZE; j ++) {
-            dest[i][j] = src[j][i];
+    for (int i = 0; i < SIZE-3; i += 4) {
+        for (int j = 0; j < SIZE-3; j += 4) {
+
+
+                for (int m = 0; m < 4; m++) {
+                        for (int n = 0; n < 4; n++) {
+                                int temp = src[i+m][j+n];
+                                dest[j+n][i+m] = temp;
+                        }
+                }
+
+                // dest[j][i] = dest00;
+                // dest[j+1][i] = dest10;
+                // dest[j][i+1] = dest01;
+                // dest[j+1][i+1] = dest10;
+        }
+    }
+    for (int i = 0; i < SIZE; i++) {
+            for (int j = 1; j < 4; j++) {
+            dest[i][SIZE-j] = src[SIZE-j][i];
+            dest[SIZE-j][i] = src[i][SIZE-j];
         }
     }
 }
